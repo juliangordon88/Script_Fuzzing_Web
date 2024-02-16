69c7a368a948126fc572ed7a0e05d4d9178cbd65
juliangordon88 <julian.gordon1988@gmail.com>
2024-02-16 12:41:40 +0100

    Importamos librerias necesarias

---
+++ Fuzzing_Web.py
@@ -0,0 +1,5 @@
+import argparse # sirve para pasar pasametros a nuestro script
+import requests # sirve para hacer peticiones http 
+from tqdm import tqdm # sirve para generar una barra de progreso del script
+
+
