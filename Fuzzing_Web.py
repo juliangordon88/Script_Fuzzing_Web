3d0781d3aff7af6c4dfc78a241d8370b61f64635
juliangordon88 <julian.gordon1988@gmail.com>
2024-02-16 12:59:16 +0100

    Generamos barra de progreso de busqueda y determinamos urls encontradas

--- Fuzzing_Web.py
+++ Fuzzing_Web.py
@@ -4,6 +4,29 @@ from tqdm import tqdm # sirve para generar una barra de progreso del script
 
 parser = argparse.ArgumentParser(description='Script de fuzzing de directorios web')
 parser.add_argument("url", type=str, help='Direccion URL a fuzzear')
-parser.add_argument("wordlist", type=str, help='Archivo de palabras a fuzzear')
+parser.add_argument("diccionario", type=str, help='Archivo de palabras a fuzzear')
 args = parser.parse_args()
+
+with open (args.diccionario) as file:
+    wordlist = file.read().splitlines()
+
+try:
+
+        barrita = tqdm(total=len(wordlist), desc="Progreso", unit="urls", dynamic_ncols=True)
+        
+        for linea in wordlist:
+            url_completa = args.url + linea
+            response = requests.get(url_completa)
+            if response.status_code == 200:
+                    tqdm.write(f"URL encontrada: {url_completa}")
+            barrita.update(1)
+
+        
+except KeyboardInterrupt:
+        print("\n Se ha interrumpido el script por pulstar Ctrl+C")
+    
+
+finally: 
+        barrita.close()
+
 
