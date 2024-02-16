2fac917854413ce39246fb4791d0ec72f82b1a0f
juliangordon88 <julian.gordon1988@gmail.com>
2024-02-16 12:45:54 +0100

    Determinamos los parametros

--- Fuzzing_Web.py
+++ Fuzzing_Web.py
@@ -2,4 +2,8 @@ import argparse # sirve para pasar pasametros a nuestro script
 import requests # sirve para hacer peticiones http 
 from tqdm import tqdm # sirve para generar una barra de progreso del script
 
+parser = argparse.ArgumentParser(description='Script de fuzzing de directorios web')
+parser.add_argument("url", type=str, help='Direccion URL a fuzzear')
+parser.add_argument("wordlist", type=str, help='Archivo de palabras a fuzzear')
+args = parser.parse_args()
 
